971c7dce6402f4485a82c8bca098033fcac82999
Wenqiang Xie <wq.acc@outlook.com>
2020-03-27 22:39:07 +0800

    10. Regular Expression Matching

---
+++ c/isMatch.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Runtime: 24 ms, faster than 25.77% of C online submissions for Regular Expression Matching.
+// Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Regular Expression Matching.
+
+bool isMatch(char *s, char *p)
+{
+    if(*p == '\0' && *s == '\0')
+        return true;
+
+    if(*p != '\0' && *(p + 1) == '*')
+    {
+        do
+        {
+            if(true == isMatch(s, p + 2))
+                return true;
+
+            if(*s == '\0')
+                break;
+
+            if(*s == *p || *p == '.')
+                s++;
+        }while(*s == *p || *p == '.');
+
+        return isMatch(s, p + 2);
+    }
+    else if((*p == '.' && *s != '\0') || (*p == *s))
+    {
+        return isMatch(s + 1, p + 1);
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int main()
+{
+    char *s = "ab", *p = ".*";
+    if(true == isMatch(s, p))
+    {
+        printf("match.");
+    }
+    else
+    {
+        printf("not match.");
+    }
+    return 0;
+}
