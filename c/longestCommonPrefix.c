40b50ab7bbbef3688f9abebe1ba6a40008ccf722
Wenqiang Xie <wq.xie@outlook.com>
2020-04-08 17:25:54 +0800

    14. Longest Common Prefix

--- c/longestCommonPrefix.c
+++ c/longestCommonPrefix.c
@@ -1,6 +1,13 @@
 // 14. Longest Common Prefix
 // https://leetcode.com/problems/longest-common-prefix/
 
+#include <stdio.h>
+
 char * longestCommonPrefix(char ** strs, int strsSize){
 
+}
+
+int main()
+{
+    return 0;
 }
