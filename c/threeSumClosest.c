6e0314620a1a7d99b7cdbcc1d009bd7b86efd757
Wenqiang Xie <wq.xie@outlook.com>
2020-04-10 16:06:59 +0800

    16. 3Sum Closest

--- c/threeSumClosest.c
+++ c/threeSumClosest.c
@@ -1,4 +1,40 @@
+// 16. 3Sum Closest
+// https://leetcode.com/problems/3sum-close
+//
+// Runtime: 364 ms, faster than 6.34% of C online submissions for 3Sum Closest.
+// Memory Usage: 5.6 MB, less than 100.00% of C online submissions for 3Sum Closest.
 
-int threeSumClosest(int* nums, int numsSize, int target){
+#include <stdio.h>
+#include <math.h>
 
+int threeSumClosest(int* nums, int numsSize, int target)
+{
+    int sum, flag = 0;
+    for(int ii = 0; ii < numsSize - 2; ii++)
+    {
+        for(int jj=ii+1; jj < numsSize - 1; jj++)
+        {
+            for(int mm = jj + 1; mm < numsSize; mm++)
+            {
+                if( 0 == flag)
+                {
+                    sum = nums[ii]+nums[jj]+nums[mm];
+                    flag = 1;
+                }
+                else
+                {
+                    sum = abs(nums[ii]+nums[jj]+nums[mm] - target) < abs(sum - target) ? nums[ii]+nums[jj]+nums[mm] : sum;
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int target = 1, closest, arr[] = {-1, 2, 1, -4};
+    closest = threeSumClosest(arr, sizeof(arr)/sizeof(int), target);
+    printf("The sum that is closest to the target is %d.", closest);
+    return 0;
 }
